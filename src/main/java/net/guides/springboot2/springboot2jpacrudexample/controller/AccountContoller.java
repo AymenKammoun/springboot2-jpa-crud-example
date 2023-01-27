fe64bf325ef2c1fbe953d9942c59aa5bd43c662a
AymenKammoun <aymen.kammoun04@gmail.com>
2023-01-27 20:20:59 +0100

    Update AccountContoller.java

--- src/main/java/net/guides/springboot2/springboot2jpacrudexample/controller/AccountContoller.java
+++ src/main/java/net/guides/springboot2/springboot2jpacrudexample/controller/AccountContoller.java
@@ -22,7 +22,7 @@ import net.guides.springboot2.springboot2jpacrudexample.exception.ResourceNotFou
 import net.guides.springboot2.springboot2jpacrudexample.model.Account;
 import net.guides.springboot2.springboot2jpacrudexample.repository.AccountRepository;
 
-@CrossOrigin(origins = "http://localhost:8080")
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("api/v1")
 public class AccountContoller {
